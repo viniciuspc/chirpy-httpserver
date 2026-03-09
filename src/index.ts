8f5820cf8f7447707bf802da6e3f31d7285ef448
Vinicius Carvalho <pasqualini9@gmail.com>
2026-03-09 09:34:45 +0000

    Add welcome page and logo.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
 
 const app = express();
+const PORT = 8080;
 
-app.listen(8080);
+app.use(express.static("."));
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
