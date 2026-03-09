ce41c7247b46e3be7bc0f99e99c0091ea9d1f34e
Vinicius Carvalho <pasqualini9@gmail.com>
2026-03-09 09:55:11 +0000

    Add healthz hanndler and restruct files to serve static files from src/app path.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+
+const handlerReadiness = (req: Request, res: Response) => {
+  res.set('Content-Type', 'text/plain; charset=utf-8');
+  res.send("OK");
+};
+
+
+app.get("/healthz", handlerReadiness);
