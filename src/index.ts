ec55643fbfffd4da3f468bef4924da4199f7399b
Vinicius Carvalho <pasqualini9@gmail.com>
2026-03-09 10:57:17 +0000

    Add admin namespace.

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,17 @@ const handlerReadiness = (req: Request, res: Response) => {
 app.get("/api/healthz", handlerReadiness);
 
 const handlerMetrics = (req: Request, res: Response) => {
-  res.set("Content-Type", "text/plain; charset=utf-8");
-  res.send(`Hits: ${config.fileserverHits}`);
+  res.set("Content-Type", "text/html; charset=utf-8");
+  res.send(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+  </body>
+</html>`);
 };
 
-app.get("/api/metrics", handlerMetrics);
+app.get("/admin/metrics", handlerMetrics);
 
 const handlerReset = (req: Request, res: Response) => {
   config.fileserverHits = 0;
@@ -36,7 +42,7 @@ const handlerReset = (req: Request, res: Response) => {
   res.send(`Hits reset to 0`);
 };
 
-app.get("/api/reset", handlerReset);
+app.get("/admin/reset", handlerReset);
 
 const middlewareLogResponses = (
   req: Request,
