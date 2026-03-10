34189a030376679686a730e17789d973070b1f6a
Vinicius Carvalho <pasqualini9@gmail.com>
2026-03-10 10:23:42 +0000

    Add validate_chirp enpoint to process and return json

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,39 @@ const handlerReset = (req: Request, res: Response) => {
 
 app.post("/admin/reset", handlerReset);
 
+async function handlerValidateChirp(req: Request, res: Response) {
+  let body = ""; // 1. Initialize
+
+  //2. Listen for data events
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  //3. Listen for end events
+  req.on("end", () => {
+    res.header("Content-Type", "application/json");
+    try {
+      const parsedBody = JSON.parse(body);
+      if(!parsedBody.body){
+        res.status(400).send(JSON.stringify({"error": "Something went wrong"})); 
+        return; 
+      }
+
+      if(parsedBody.body.length > 140) {
+        res.status(400).send(JSON.stringify({"error": "Chirp is too long"})); 
+        return; 
+      } else {
+         res.status(200).send(JSON.stringify({"valid": true})); 
+        return; 
+      }
+    } catch (error) {
+     res.status(400).send(JSON.stringify({"error": "Something went wrong"})); 
+    }
+  });
+}
+
+app.post("/api/validate_chirp", handlerValidateChirp);
+
 const middlewareLogResponses = (
   req: Request,
   res: Response,
