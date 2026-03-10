c3fcf6c28080af58e990a86f6a44b521b448396b
Vinicius Carvalho <pasqualini9@gmail.com>
2026-03-10 12:08:07 +0000

    Initial drizzle config and users migration.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,22 @@
+
+process.loadEnvFile();
+
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
-export const config: APIConfig = {fileserverHits: 0};
+export const config: APIConfig = {
+  fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL")
+};
+
+function envOrThrow(key: string){
+  const value = process.env[key];
+  if(!value){
+    throw `Variable with key ${key} not found in the environment.`;
+  }
+
+  return value;
+  
+}
