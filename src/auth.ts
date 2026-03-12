c7a460e602bbca30c5ac034407a9d0fed3ab498e
Vinicius Carvalho <pasqualini9@gmail.com>
2026-03-12 18:02:55 +0000

    Add jwt functions and unitests.

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,9 @@
 import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
+import { ForbiddenError } from "./api/errors.js";
 
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
 
 export async function hashPassword(password: string): Promise<string> {
   return await argon2.hash(password);
@@ -7,4 +11,32 @@ export async function hashPassword(password: string): Promise<string> {
 
 export async function checkPasswordHash(password: string, hash: string): Promise<boolean> {
   return await argon2.verify(hash, password);
+}
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string{
+  const iat = Math.floor(Date.now() / 1000);
+
+  const payload: payload = {
+    iss: "chirpy",
+    sub: userID,
+    iat: iat,
+    exp: iat + expiresIn
+  }
+
+  return jwt.sign(payload, secret);
+
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+  try {
+    const verifiedJWT = jwt.verify(tokenString, secret);
+    if(typeof verifiedJWT.sub === "string") {
+      return verifiedJWT.sub;
+    } else {
+      return "";
+    }
+  } catch (err) {
+    throw new ForbiddenError(`JWT token is invalid or has expired: ${tokenString}`);
+  }
+
 }
---
+++ src/test/auth.test.ts
@@ -0,0 +1,53 @@
+import { describe, it, expect, beforeAll } from "vitest";
+import {
+  makeJWT,
+  validateJWT,
+  hashPassword,
+  checkPasswordHash,
+} from "../auth.js";
+import { ForbiddenError } from "../api/errors.js";
+
+describe("Password Hashing", () => {
+  const password1 = "correctPassword123!";
+  const password2 = "anotherPassword456!";
+  let hash1: string;
+  let hash2: string;
+
+  beforeAll(async () => {
+    hash1 = await hashPassword(password1);
+    hash2 = await hashPassword(password2);
+  });
+
+  it("should return true for the correct password", async () => {
+    const result = await checkPasswordHash(password1, hash1);
+    expect(result).toBe(true);
+  });
+});
+
+describe("Create JWT", () => {
+  const userId = "123456";
+  const expiresIn = 1000;
+  const signature = "supersecretsignature";
+
+  const expiredId = "7890"
+  const expiresInNow = 0;
+  const expiredSignature = "expiresupersecretsignature"
+
+  let tokenString: string;
+  let expiredString: string;
+
+  beforeAll(async () => {
+    tokenString = makeJWT(userId, expiresIn, signature);
+    expiredString = makeJWT(expiredId, expiresInNow, expiredSignature);
+  });
+
+  it("should return userId to a valid JWT", async () => {
+    const result = validateJWT(tokenString, signature);
+    expect(result).toBe(userId);
+  });
+
+  it("should throw exception for an expired token", async () => {
+    expect(() => validateJWT(expiredString, expiredSignature)).toThrow(ForbiddenError);
+    
+  });
+});
